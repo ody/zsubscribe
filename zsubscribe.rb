382a2b4d191f0bdcca4ef5bd216d0daa99b97bc2
Cody <ody@herriges.org>
2010-04-17 02:22:33 -0700

    Class that actually does work.

--- zsubscribe.rb
+++ zsubscribe.rb
@@ -5,16 +5,40 @@ class Report
     @filesystem = filesystem
   end
   def summary
-    @summary = %x{/usr/sbin/zfs get -H -o property,value quota,refquota,reservation,refreservation #{@filesystem}}.split(/\n/)
+    @hashed = {}
+    @summary = %x{/usr/sbin/zfs get -H -o property,value quota,refquota,reserv,refreserv #{@filesystem}}.split(/\n/)
     @summary.each do |element| 
       @pieces = element.split(/\t/)
-      if  @pieces[0] =~ /ref.*/ and @pieces[1] != "none"
-        @refed = true
-      end
+      @hashed[@pieces[0]] = @pieces[1]
     end
-  return @refed
+  return @hashed 
   end
 end
 
-mycheck = Report.new(ARGV)
-puts mycheck.summary
+class Refit
+  def initialize(filesystem, hashed)
+    @filesystem = filesystem
+    @hashed = hashed
+  end
+  def Convert
+   %x{/usr/sbin/zfs set refquota=#{@hashed["quota"]} #{@filesystem}}
+   %x{/usr/sbin/zfs set refreserv=#{@hashed["reserv"]} #{@filesystem}}
+   %x{/usr/sbin/zfs set quota=none #{@filesystem}}
+   %x{/usr/sbin/zfs set reserv=none #{@filesystem}}
+  end  
+end
+
+
+myreport = Report.new(ARGV)
+hashed = myreport.summary
+if hashed['quota'] != "none" and hashed['refquota'] == "none"
+  print "Not Referenced\nShall I convert it to a referenced quota and reservation? "
+  a = STDIN.gets.chomp
+  if a == "y" or a == "yes"
+    myrefit = Refit.new(ARGV, hashed)
+  elsif a == "n" or a == "no"
+    puts "Not fixing\nExited!"
+  else
+    puts "Valid answers are Yes/No"
+  end 
+end
