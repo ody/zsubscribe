7c852a750cb12d66740ffa62e525bdaf85fbd5a0
Cody <ody@herriges.org>
2010-04-16 20:57:40 -0700

    Understanding proper regex matching.

--- zsubscribe.rb
+++ zsubscribe.rb
@@ -1,20 +1,23 @@
 #!/usr/bin/env ruby
 
-class Report
-  def initialize(filesystem)
-    @filesystem = filesystem
-  end
-  def summary
+#class Report
+#  def initialize(filesystem)
+#    @filesystem = filesystem
+#  end
+#  def summary(filesystem)
+    @filesystem = ARGV
     @summary = %x{/usr/sbin/zfs get -H -o property,value quota,refquota,reservation,refreservation #{@filesystem}}.split(/\n/)
+    @refed = false
     @summary.each do |element| 
-      if element != "none" 
-        @nonone = element + "\n" + @nonone.to_s
+      @pieces = element.split(/\t/)
+      if  @pieces[0] =~ /ref.*/ and @pieces[1] != "none"
+        @refed = true
+        puts "refed"
       end
     end
-    return @nonone
-  end
-end
+  #end
+#end
 
-mycheck = Report.new(ARGV)
-puts mycheck.summary.inspect
+#mycheck = Report.new(ARGV)
+#puts summary(ARGV)
 
