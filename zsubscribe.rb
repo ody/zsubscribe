a16bfd113920ee1da2e10669c5a18b6448043941
Cody A Herriges <ody@bunny.cat.pdx.edu>
2010-04-16 20:15:20 -0700

    Breaking stuff into arrays to do processing.

--- zsubscribe.rb
+++ zsubscribe.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
-class report
-  def getdata(filesystem)
+class Report
+  def initialize(filesystem)
     @filesystem = filesystem
-    summary = system("/usr/sbin/zfs get 
+  end
+  def summary
+    @summary = %x{/usr/sbin/zfs get -H -o property,value quota,refquota,reservation,refreservation #{@filesystem}}.split(/\n/)
+    @summary.each do |element| 
+      if element != "none" 
+        @nonone = element + "\n" + @nonone.to_s
+      end
+    end
+    return @nonone
+  end
+end
+
+mycheck = Report.new(ARGV)
+puts mycheck.summary.inspect
+
