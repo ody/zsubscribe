6a40daccfd06c1a45a6d56135e193b6706d77467
Cody Herriges <ody@matsumoto.(none)>
2010-04-20 01:06:50 -0700

    Adding math.

--- zsubscribe.rb
+++ zsubscribe.rb
@@ -6,7 +6,7 @@ class Report
   end
   def summary
     @hashed = {}
-    @summary = %x{/usr/sbin/zfs get -H -o property,value quota,refquota,reserv,refreserv #{@filesystem}}.split(/\n/)
+    @summary = %x{/usr/sbin/zfs get -p -H -o property,value quota,refquota,reserv,refreserv,referenced #{@filesystem}}.split(/\n/)
     @summary.each do |element| 
       @pieces = element.split(/\t/)
       @hashed[@pieces[0]] = @pieces[1]
@@ -20,14 +20,31 @@ class Refit
     @filesystem = filesystem
     @hashed = hashed
   end
-  def Convert
-   %x{/usr/sbin/zfs set refquota=#{@hashed["quota"]} #{@filesystem}}
-   %x{/usr/sbin/zfs set refreserv=#{@hashed["reserv"]} #{@filesystem}}
-   %x{/usr/sbin/zfs set quota=none #{@filesystem}}
-   %x{/usr/sbin/zfs set reserv=none #{@filesystem}}
+  def convert
+    %x{/usr/sbin/zfs set refquota=#{@hashed["quota"]} #{@filesystem}}
+    %x{/usr/sbin/zfs set refreserv=#{@hashed["reservation"]} #{@filesystem}}
+    %x{/usr/sbin/zfs set quota=none #{@filesystem}}
+    %x{/usr/sbin/zfs set reserv=none #{@filesystem}}
+    return 0
   end  
 end
 
+class Math
+  def initialize(filesystem, hashed)
+    @filesystem = filesystem
+    @hashed = hashed
+  end
+  def ratios
+    @realuse = @hashed["referenced"].quo(@hashed["refreserv"])
+    if @realuse < .5
+      return 0
+    elsif @realuse = (0.5..0.85) 
+      return 1
+    else
+      return 2
+    end
+  end
+end
 
 myreport = Report.new(ARGV)
 hashed = myreport.summary
@@ -36,6 +53,7 @@ if hashed['quota'] != "none" and hashed['refquota'] == "none"
   a = STDIN.gets.chomp
   if a == "y" or a == "yes"
     myrefit = Refit.new(ARGV, hashed)
+    myrefit.convert
   elsif a == "n" or a == "no"
     puts "Not fixing\nExited!"
   else
